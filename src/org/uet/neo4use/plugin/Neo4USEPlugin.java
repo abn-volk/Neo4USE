d51ad20823224b53b8807b382e3a6a5b7abc9b49
pnh <pnh@125.235.4.59.adsl.viettel.vn>
2017-09-05 22:47:36 +0700

    Add plug-in stubs

---
+++ src/org/uet/neo4use/plugin/Neo4USEPlugin.java
@@ -0,0 +1,17 @@
+package org.uet.neo4use.plugin;
+
+import org.tzi.use.runtime.IPlugin;
+import org.tzi.use.runtime.IPluginRuntime;
+
+public class Neo4USEPlugin implements IPlugin{
+
+	@Override
+	public String getName() {
+		return "Neo4USE";
+	}
+
+	@Override
+	public void run(IPluginRuntime pluginRuntime) throws Exception {		
+	}
+
+}
