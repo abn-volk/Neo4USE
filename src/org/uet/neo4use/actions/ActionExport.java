1fc9a840ccac18eb56b5dca962e5924fede2f44e
PNH <pnhoang96@gmail.com>
2017-09-29 15:57:25 +0700

    Add modal dialog for export progress

--- src/org/uet/neo4use/actions/ActionExport.java
+++ src/org/uet/neo4use/actions/ActionExport.java
@@ -3,7 +3,11 @@ package org.uet.neo4use.actions;
 import java.io.File;
 import java.io.PrintWriter;
 
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
+import javax.swing.WindowConstants;
 
 import org.tzi.use.config.Options;
 import org.tzi.use.gui.main.MainWindow;
@@ -11,11 +15,12 @@ import org.tzi.use.runtime.gui.IPluginAction;
 import org.tzi.use.runtime.gui.IPluginActionDelegate;
 
 public class ActionExport implements IPluginActionDelegate {
-	private File selectedFile;
-
+	
+	private MainWindow fMainWindow;
+	
 	@Override
 	public void performAction(IPluginAction pluginAction) {
-		MainWindow fMainWindow = pluginAction.getParent();
+		fMainWindow = pluginAction.getParent();
 		String path = Options.getLastDirectory().toString();
 		JFileChooser fChooser = new JFileChooser(path);
 		PrintWriter fLogWriter = fMainWindow.logWriter();
@@ -28,8 +33,38 @@ public class ActionExport implements IPluginActionDelegate {
 		}
 		path = fChooser.getSelectedFile().toString();
 		Options.setLastDirectory(new File(path).toPath());
-		selectedFile = fChooser.getSelectedFile();
-		fLogWriter.printf("Selected path is %s", path);
+		File selectedFile = fChooser.getSelectedFile();
+		fLogWriter.printf("Selected path is %s", path);	
+		performExport(selectedFile);
+	}
+
+	private void performExport(File selectedFile) {
+		String message = String.format("Exporting model into %s. Please wait.", selectedFile.getPath());
+		JOptionPane pane = new JOptionPane(message, JOptionPane.WARNING_MESSAGE, 
+				JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
+		JDialog dialog = pane.createDialog("Exporting...");
+		dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		dialog.setVisible(true);
+		ExportTask task = new ExportTask(selectedFile, dialog);
+		task.execute();
+		
+	}
+	
+	private class ExportTask extends SwingWorker<Void, Void> {
+		
+		private File file;
+		private JDialog dialog;
+
+		ExportTask (File file, JDialog dialog) {
+			this.file = file;
+			this.dialog = dialog;
+		}
+		
+		@Override
+		protected Void doInBackground() throws Exception {			
+			return null;
+		}
+		
 	}
 
 }
