cb42e92adaeb3d3d06f99e4217bc8d28bf207c20
PNH <pnhoang96@gmail.com>
2017-09-29 16:29:05 +0700

    Add worker thread and dialogs

--- src/org/uet/neo4use/actions/ActionExport.java
+++ src/org/uet/neo4use/actions/ActionExport.java
@@ -1,7 +1,9 @@
 package org.uet.neo4use.actions;
 
+import java.awt.EventQueue;
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.concurrent.ExecutionException;
 
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -9,6 +11,8 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 import javax.swing.WindowConstants;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.tzi.use.config.Options;
 import org.tzi.use.gui.main.MainWindow;
 import org.tzi.use.runtime.gui.IPluginAction;
@@ -17,13 +21,14 @@ import org.tzi.use.runtime.gui.IPluginActionDelegate;
 public class ActionExport implements IPluginActionDelegate {
 	
 	private MainWindow fMainWindow;
+	private PrintWriter fLogWriter;
 	
 	@Override
 	public void performAction(IPluginAction pluginAction) {
 		fMainWindow = pluginAction.getParent();
+		fLogWriter = fMainWindow.logWriter();
 		String path = Options.getLastDirectory().toString();
 		JFileChooser fChooser = new JFileChooser(path);
-		PrintWriter fLogWriter = fMainWindow.logWriter();
 		fChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		fChooser.setCurrentDirectory(new File("."));
 		fChooser.setDialogTitle("Select folder");
@@ -34,7 +39,6 @@ public class ActionExport implements IPluginActionDelegate {
 		path = fChooser.getSelectedFile().toString();
 		Options.setLastDirectory(new File(path).toPath());
 		File selectedFile = fChooser.getSelectedFile();
-		fLogWriter.printf("Selected path is %s", path);	
 		performExport(selectedFile);
 	}
 
@@ -44,14 +48,12 @@ public class ActionExport implements IPluginActionDelegate {
 				JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
 		JDialog dialog = pane.createDialog("Exporting...");
 		dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-		dialog.setVisible(true);
 		ExportTask task = new ExportTask(selectedFile, dialog);
 		task.execute();
-		
 	}
 	
-	private class ExportTask extends SwingWorker<Void, Void> {
-		
+	private class ExportTask extends SwingWorker<Boolean, Void> {
+
 		private File file;
 		private JDialog dialog;
 
@@ -61,8 +63,39 @@ public class ActionExport implements IPluginActionDelegate {
 		}
 		
 		@Override
-		protected Void doInBackground() throws Exception {			
-			return null;
+		protected Boolean doInBackground() throws Exception {
+			EventQueue.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					dialog.setVisible(true);
+				}
+			});
+			GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(file);
+			// TODO: Implement exporting here
+			return true;
+		}
+		
+		@Override
+		protected void done() {
+			super.done();
+			dialog.setVisible(false);
+			dialog.dispose();
+			try {
+				if (get())
+					JOptionPane.showMessageDialog(fMainWindow, 
+							String.format("Model exported to %s.", file.getPath()), "Success",
+							JOptionPane.INFORMATION_MESSAGE);
+				else 
+					JOptionPane.showMessageDialog(fMainWindow, 
+							"Unexpected errors occurred during export. See log pane for details.", "Export error",
+							JOptionPane.ERROR_MESSAGE);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
