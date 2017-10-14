da65b20c856cf0d07ea164924b49674b7acff239
PNH <pnhoang96@gmail.com>
2017-10-14 21:16:35 +0700

    Import link, link objects and references.

--- src/org/uet/neo4use/actions/ActionImport.java
+++ src/org/uet/neo4use/actions/ActionImport.java
@@ -3,7 +3,12 @@ package org.uet.neo4use.actions;
 import java.awt.EventQueue;
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
@@ -13,9 +18,11 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 import javax.swing.WindowConstants;
 
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -25,10 +32,11 @@ import org.tzi.use.config.Options;
 import org.tzi.use.gui.main.MainWindow;
 import org.tzi.use.runtime.gui.IPluginAction;
 import org.tzi.use.runtime.gui.IPluginActionDelegate;
+import org.tzi.use.uml.mm.MAssociation;
+import org.tzi.use.uml.mm.MAssociationEnd;
 import org.tzi.use.uml.ocl.type.CollectionType;
 import org.tzi.use.uml.ocl.type.Type;
 import org.tzi.use.uml.ocl.type.Type.VoidHandling;
-import org.tzi.use.uml.ocl.value.IntegerValue;
 import org.tzi.use.uml.sys.MObject;
 
 public class ActionImport implements IPluginActionDelegate {
@@ -86,8 +94,11 @@ public class ActionImport implements IPluginActionDelegate {
 				}
 			});
 			graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(file);
-			boolean res = importObjects(graphDb);
-			return res;
+			if (importObjects(graphDb))
+				if (importLinkObjects(graphDb))
+					if (importLinks(graphDb))
+						return true;
+			return false;
 		}
 		
 		@Override
@@ -119,37 +130,155 @@ public class ActionImport implements IPluginActionDelegate {
 				ResourceIterator<Node> objNodes = graphDb.findNodes(Label.label("__Object"));
 				while (objNodes.hasNext()) {
 					Node node = objNodes.next();
-					String cls = getOriginalClass(node, "__Object");
-					if (cls == null) {
-						fLogWriter.println(String.format("Error: Node with id %ld does not have a valid label representing its class", node.getId()));
+					if (!importObject(node)) return false;
+				}
+				tx.success();
+			}
+			try (Transaction tx = graphDb.beginTx()) {
+				ResourceIterator<Node> objNodes = graphDb.findNodes(Label.label("__Object"));
+				while (objNodes.hasNext()) {
+					Node node = objNodes.next();
+					if (!importReferences(node)) return false;
+				}
+				tx.success();
+			}
+			return true;
+		}
+		
+		private boolean importLinks(GraphDatabaseService graphDb) {
+			try (Transaction tx = graphDb.beginTx()) {
+				ResourceIterator<Node> linkNodes = graphDb.findNodes(Label.label("__Link"));
+				while (linkNodes.hasNext()) {
+					Node node = linkNodes.next();
+					if (!importLink(node, false)) return false;
+				}
+				tx.success();
+			}
+			return true;
+		}
+		
+		private boolean importLinkObjects(GraphDatabaseService graphDb) {
+			try (Transaction tx = graphDb.beginTx()) {
+				ResourceIterator<Node> linkObjNodes = graphDb.findNodes(Label.label("__LinkObject"));
+				while (linkObjNodes.hasNext()) {
+					Node node = linkObjNodes.next();
+					if (!importLink(node, true)) return false;
+				}
+				tx.success();
+			}
+			try (Transaction tx = graphDb.beginTx()) {
+				ResourceIterator<Node> objNodes = graphDb.findNodes(Label.label("__Object"));
+				while (objNodes.hasNext()) {
+					Node node = objNodes.next();
+					if (!importReferences(node)) return false;
+				}
+				tx.success();
+			}
+			return true;
+		}
+		
+		private boolean importObject(Node node) {
+			String cls = getOriginalLabel(node, "__Object");
+			if (cls == null) {
+				fLogWriter.println(String.format("Error: Node with id %d does not have a valid label representing its class", node.getId()));
+				return false;
+			}
+			Object useName = node.getProperty("__name", null);
+			if (useName == null || !(useName instanceof String)) {
+				fLogWriter.println(String.format("Error: Node with id %d must have the __name attribute with a string value!", node.getId()));
+				return false;
+			}
+			String name = (String) useName;
+			fLogWriter.println(String.format("Importing %s", name));
+			try {
+				MObject obj = fSystemApi.createObject(cls, name);
+				if (!importAttributes(obj, node)) return false;
+			}
+			catch (UseApiException e) {
+				fLogWriter.println(String.format("Error: %s", e.getMessage()));
+				return false;
+			}
+			return true;
+		}
+		
+		private boolean importAttributes(MObject obj, Node node) {
+			// Import primitive attributes
+			for (String key : node.getPropertyKeys()) {
+				if (!key.equals("__name")) {
+					boolean res = importAttribute(obj, key, node.getProperty(key));
+					if (!res) return false;
+				}
+			}
+			return true;
+		}
+		
+		private boolean importReferences(Node node) {
+			Object useName = node.getProperty("__name", null);
+			if (useName == null || !(useName instanceof String)) {
+				fLogWriter.println(String.format("Error: Node with id %d must have the __name attribute with a string value!", node.getId()));
+				return false;
+			}
+			String name = (String) useName;
+			MObject obj = fSystemApi.getObject(name); 
+			if (obj == null) {
+				fLogWriter.println(String.format("Error when importing %s's references: Object not found.", name));
+				return false;
+			}
+			// Import object and collections of object
+			Iterable<Relationship> relas = node.getRelationships(Direction.OUTGOING);
+			Map<String, ArrayList<Relationship>> refs = new HashMap<String, ArrayList<Relationship>>();
+			for (Relationship rela : relas) {
+				// A single object reference
+				if (!rela.hasProperty("__index")) {
+					String attrName = rela.getType().name();
+					Object objName = rela.getEndNode().getProperty("__name");
+					if (objName == null || !(objName instanceof String)) {
+						fLogWriter.println(String.format("Error: Node id %d has invalid __name.", rela.getEndNode().getId()));
 						return false;
 					}
-					Object useName = node.getProperty("__name", null);
-					if (useName == null) {
-						fLogWriter.println(String.format("Error: Node with id %ld does not have the __name attribute.", node.getId()));
-						return false;
-					}
-					if (!(useName instanceof String)) {
-						fLogWriter.println(String.format("Error: The __name attribute of the node with id %ld is supposed to be a string!", node.getId()));
-						return false;
-					}
-					String name = (String) useName;
-					fLogWriter.println(String.format("Importing %s", name));
 					try {
-						MObject obj = fSystemApi.createObject(cls, name);
-						for (String key : node.getPropertyKeys()) {
-							fLogWriter.println(key);
-							if (!key.equals("__name")) {
-								boolean res = importAttribute(obj, key, node.getProperty(key));
-								if (!res) return false;
-							}
-						}
+						fSystemApi.setAttributeValue(name, attrName, objName.toString());
 					}
 					catch (UseApiException e) {
+						fLogWriter.println(String.format("Error when assigning %s.%s: %s", name, objName.toString(), e.getMessage()));
 						return false;
 					}
 				}
-				tx.success();
+				// Collections of objects
+				else {
+					String type = rela.getType().name();
+					if (rela.hasProperty("__index") && rela.getProperty("__index") instanceof Integer) {
+						if (refs.get(type) == null) 
+							refs.put(type, new ArrayList<Relationship>());
+						refs.get(type).add(rela);
+					}
+					else {
+						fLogWriter.println(String.format("Error when assigning %s.%s: Relationship must have a valid __index property.", name, type));
+						return false;
+					}
+				}
+			}
+			// Assign collection attributes
+			for (String attrName : refs.keySet()) {
+				List<Relationship> relaList = refs.get(attrName);
+				relaList.sort(new Comparator<Relationship>() {
+					@Override
+					public int compare(Relationship arg0, Relationship arg1) {
+						return ((Integer) arg0.getProperty("__index")) - ((Integer) arg1.getProperty("__index"));
+					}
+				});
+				try {
+					String body = relaList.stream().map(n -> n.getEndNode().getProperty("__name").toString()).collect(Collectors.joining(","));
+					fSystemApi.setAttributeValue(name, attrName, String.format("%s{%s}", getCollectionType(obj, attrName), body));
+				}
+				catch (NullPointerException e) {
+					fLogWriter.println(String.format("Error when assigning %s.%s: One of the nodes doesn't have a valid __name.", obj.name(), attrName));
+					return false;
+				}
+				catch (UseApiException e) {
+					fLogWriter.println(String.format("Error when assigning %s.%s: %s", obj.name(), attrName, e.getMessage()));
+					return false;
+				}
 			}
 			return true;
 		}
@@ -198,6 +327,60 @@ public class ActionImport implements IPluginActionDelegate {
 			}
 			catch (UseApiException e) {
 				fLogWriter.println(String.format("Error when assigning %s.%s: %s", objName, name, e.getMessage()));
+				return false;
+			}
+			return true;
+		}
+		
+		private boolean importLink(Node node, boolean isLinkObj) {
+			String qualifierLabel = isLinkObj? "__LinkObject" : "__Link";
+			String label = getOriginalLabel(node, qualifierLabel);
+			if (label == null) {
+				fLogWriter.println(String.format("Error: Node with id %d does not have a valid label representing its association type.", node.getId()));
+				return false;
+			}
+			MAssociation asc = fSystemApi.getSystem().model().getAssociation(label);
+			if (asc == null) {
+				fLogWriter.println(String.format("Error: No association found for %s.", label));
+				return false;
+			}
+			// Map relationships to link ends in the correct order (determined by rolenames)
+			List<MAssociationEnd> ends = asc.associationEnds();
+			Iterable<Relationship> relas = node.getRelationships(Direction.OUTGOING);
+			Map<String, String> role2obj = new HashMap<>(ends.size());
+			List<String> objNames = new ArrayList<>(ends.size());
+			for (Relationship rela : relas ) {
+				Object name = rela.getEndNode().getProperty("__name");
+				if (name == null || !(name instanceof String)) {
+					fLogWriter.println(String.format("Error: Node id %d has invalid __name.", rela.getEndNode().getId()));
+					return false;
+				}
+				role2obj.put(rela.getType().name(), rela.getEndNode().getProperty("__name").toString());
+			}
+			for (MAssociationEnd end: ends) {
+				String objName = role2obj.getOrDefault(end.name(), null);
+				if (objName == null) {
+					fLogWriter.println(String.format("Error: No object name found for rolename %s.", end.name()));
+					return false;
+				}
+				objNames.add(objName);
+			}
+			try {
+				String[] connectedObjectNames = objNames.toArray(new String[objNames.size()]);
+				if (isLinkObj) {
+					Object useName = node.getProperty("__name", null);
+					if (useName == null || !(useName instanceof String)) {
+						fLogWriter.println(String.format("Error: Node with id %d must have the __name attribute with a string value!", node.getId()));
+						return false;
+					}
+					MObject obj = fSystemApi.createLinkObject(label, useName.toString(), connectedObjectNames);
+					if (!importAttributes(obj, node)) return false;
+				}
+				else
+					fSystemApi.createLink(label, connectedObjectNames);
+			} catch (UseApiException e) {
+				fLogWriter.println(String.format("Error: Exception when creating link or link object: %s", e.getMessage()));
+				return false;
 			}
 			return true;
 		}
@@ -210,10 +393,12 @@ public class ActionImport implements IPluginActionDelegate {
 				return "OrderedSet";
 			else if (type.isKindOfSet(VoidHandling.EXCLUDE_VOID))
 				return "Set";
-			else return "Sequence";
+			else if (type.isKindOfSequence(VoidHandling.EXCLUDE_VOID))
+				return "Sequence";
+			else return null;
 		}
 				
-		private String getOriginalClass(Node node, String otherLabel) {
+		private String getOriginalLabel(Node node, String otherLabel) {
 			for (Label l : node.getLabels()) {
 				if (!l.name().equals(otherLabel)) {
 					return l.name();
